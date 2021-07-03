04a89f03ac56dfb36d58d5dfdb18c67738adf220
kytay <->
2021-07-03 14:52:01 +0800

    Update

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 cd ./docker-swag && \
 git submodule init && git submodule update && \
-cp ../scripts_ext/app/* root/app && \
 cp ../scripts_ext/cont-init.d/* root/etc/cont-init.d && \
 cp ../scripts_ext/cont-init.d/80-digitalocean-dns-updater root/app/do-dnsupdate.sh && \
 docker build \
