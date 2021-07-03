efcda17c783bf87daf8dbfba968cd320eefd67aa
kytay <->
2021-07-03 16:09:26 +0800

    Intial commit

--- build-run.sh
+++ build-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./build.sh && \
-docker run --env-file=./.secret -it --rm swag_multidomain:latest
+docker run --env-file=./.secret -it --rm kytay/swag_multidomain:latest
--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 cd ./docker-swag && \
 git submodule init && git submodule update && \
-cp ../scripts_ext/cont-init.d/* root/etc/cont-init.d && \
-cp ../scripts_ext/cont-init.d/80-digitalocean-dns-updater root/app/do-dnsupdate.sh && \
+cp -f ../scripts_ext/cont-init.d/* root/etc/cont-init.d && \
+cp -f ../scripts_ext/cont-init.d/80-digitalocean-dns-updater root/app/do-dnsupdate.sh && \
+
 docker build \
   --no-cache \
   --pull \
-  -t swag_multidomain:latest .
+  -t kytay/swag_multidomain:latest .
+
+docker tag kytay/swag_multidomain:latest kytay/swag_multidomain:$(date --rfc-3339=date)
+docker push -a kytay/swag_multidomain
