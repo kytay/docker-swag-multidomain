1eefbb839afb41a2558b2085695b04337c662a98
kytay <->
2021-07-04 13:34:29 +0800

    Update build-run

--- build-run.sh
+++ build-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./build.sh && \
-docker run --env-file=./.secret -it --rm kytay/swag_multidomain:latest
+docker run --env-file=./.secret -it -v $(pwd)/container-vol:/config --rm kytay/swag_multidomain:latest
