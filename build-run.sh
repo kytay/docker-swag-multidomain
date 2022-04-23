96c4ced4135a3584035723f6837b64958d2db6d2
kytay <->
2022-04-24 00:47:14 +0800

    Update cloudflare ddns updater

--- build-run.sh
+++ build-run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+IMAGE_NAME=swag_cloudflare
+
 ./build.sh && \
-docker run --env-file=./.secret -it -v $(pwd)/container-vol:/config --rm kytay/swag_multidomain:latest
+docker run --env-file=./.secret -it -v $(pwd)/container-vol:/config --rm kytay/${IMAGE_NAME}:latest /bin/bash
--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+IMAGE_NAME=swag_cloudflare
+
 cd ./docker-swag && \
-rm $(git status -s | awk '{print $2}' | tr '\n' ' ') 
+rm -Rf $(git status -s | awk '{print $2}' | tr '\n' ' ') 
 git submodule init && \
 git submodule update && \
-cp -f ../scripts_ext/cont-init.d/* root/etc/cont-init.d
+cp -rf ../scripts_ext/app/* root/app
+cp -rf ../scripts_ext/cont-init.d/* root/etc/cont-init.d
 
 docker build \
   --no-cache \
   --pull \
-  -t kytay/swag_multidomain:latest .
+  -t kytay/${IMAGE_NAME}:latest .
 
-docker tag kytay/swag_multidomain:latest kytay/swag_multidomain:$(date --rfc-3339=date)
-docker push -a kytay/swag_multidomain
+docker tag kytay/${IMAGE_NAME}:latest kytay/${IMAGE_NAME}:$(date --rfc-3339=date)
+docker push -a kytay/${IMAGE_NAME}
